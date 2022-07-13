334fcb204690a3ce2f2adb7f146b7188849127fa
mesh123 <mesh.krishna@gmail.com>
2022-07-13 12:49:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+    return (
+     <div>
+        <Menu dishes={this.state.dishes} />
+     </div>
+    );
+  }
+}
+
+export default App;
